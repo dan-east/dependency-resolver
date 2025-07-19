055c1d19cac1543642ba35a4c4bbe9a5c0631419
Dan <east_dan@hotmail.com>
2025-07-19 06:42:50 +0100

    Updated constants to make setting a runtime directory easier

--- dependency_resolver/constants.py
+++ dependency_resolver/constants.py
@@ -3,6 +3,9 @@ import os
 # Defines where the home directory is - can be overridden
 HOME_DIR = os.getenv("RESOLVER_HOME", os.getcwd())
 
+# Default runtime directory
+RUNTIME_DIR:str = os.getenv("RESOLVER_RUNTIME_DIR", f"{HOME_DIR}/dependency-resolver-runtime")
 
-LOG_DIR = os.getenv("RESOLVER_LOG_DIR", os.getcwd())
+# Logging constants
+LOG_DIR = os.getenv("RESOLVER_LOG_DIR", RUNTIME_DIR)
 LOG_TO_FILE=f"{LOG_DIR}/resolver.log"
